7ba2bf23e144cf0edbfcac44cc3b2f80170184c3
huangchunhao <2015huangchunhao>
2019-08-04 18:11:02 +0800

    json

---
+++ jsonutil/JSONUtil.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/4 0004 17:04
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : JSONUtil.py
+# @Software: PyCharm
+
+import json
+
+class JSONUtil:
+
+    def jTop(self,str="{}"):
+        json
